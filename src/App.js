3025f3385c4a0f45f9617afac2d4fee647026abb
Rachael Drennan <rdrennan0@gmail.com>
2020-07-25 12:40:26 -0600

    add homepage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import comingSoon from "./ComingSoon.png";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <img src={comingSoon} className="coming-Soon" alt="Coming Soon!" />
     </div>
   );
 }
