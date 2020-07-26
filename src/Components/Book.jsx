2e88e1c2c21e2002654f36a8cdfcf61dac7c1da8
Rachael Drennan <rdrennan0@gmail.com>
2020-07-26 13:18:19 -0600

    update picture on book

--- src/Components/Book.jsx
+++ src/Components/Book.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Call from "../Images/ShelsSide.jpg";
+import Call from "../Images/bookings.jpg";
 import { makeStyles } from "@material-ui/core/styles";
 
 import Paper from "@material-ui/core/Paper";
