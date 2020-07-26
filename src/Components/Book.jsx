b6be363fd918c8f656a9f2caaabc574c78f33a6d
Rachael Drennan <rdrennan0@gmail.com>
2020-07-26 12:51:48 -0600

    linter

--- src/Components/Book.jsx
+++ src/Components/Book.jsx
@@ -4,7 +4,6 @@ import { makeStyles } from "@material-ui/core/styles";
 
 import Paper from "@material-ui/core/Paper";
 
-import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
