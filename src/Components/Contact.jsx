d06a7d9a3635aa0d313ca62da7f4fbcb4229d29b
Rachael Drennan <rdrennan0@gmail.com>
2020-07-26 13:23:58 -0600

    update hours

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -3,13 +3,11 @@ import FacebookIcon from "@material-ui/icons/Facebook";
 import InstagramIcon from "@material-ui/icons/Instagram";
 import Button from "@material-ui/core/Button";
 import AttachMoneyIcon from "@material-ui/icons/AttachMoney";
-import Location from "../Images/location.jpg";
 import Hours from "./Hours";
 
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 
 import { makeStyles } from "@material-ui/core/styles";
@@ -34,7 +32,6 @@ const useStyles = makeStyles({
 export default function Contact() {
   const classes = useStyles();
 
-
   return (
     <div>
       <div style={{ width: "100%" }}></div>
@@ -42,6 +39,7 @@ export default function Contact() {
         <Card className={classes.root}>
           <CardActionArea>
             <iframe
+              title="Map"
               style={{
                 width: "300px",
                 height: "300px",
