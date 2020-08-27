2f78af8f6930675673a62ac57d241b87eced408d
Rachael Drennan <rdrennan0@gmail.com>
2020-08-26 19:32:34 -0600

    remove unused imports

--- src/Components/Grid.jsx
+++ src/Components/Grid.jsx
@@ -1,11 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
-import FormLabel from "@material-ui/core/FormLabel";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import RadioGroup from "@material-ui/core/RadioGroup";
-import Radio from "@material-ui/core/Radio";
-import Paper from "@material-ui/core/Paper";
 import CardMedia from "@material-ui/core/CardMedia";
 
 const useStyles = makeStyles((theme) => ({
